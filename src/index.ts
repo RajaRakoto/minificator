88df4394ef3e146dbacdf40d84cf6eb0a94ae946
raja <raja.rakoto7@gmail.com>
2024-02-27 18:49:06 +0300

    src: init inquirer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-export const one = 1;
-export const two = 2;
-console.log('Hello via Bun!');
+import inquirer from 'inquirer';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Enter your name:',
+  },
+  {
+    type: 'list',
+    name: 'language',
+    message: 'Select a preferred language:',
+    choices: ['JavaScript', 'TypeScript', 'Python'],
+  },
+];
+
+async function main(): Promise<void> {
+  const answers = await inquirer.prompt(questions);
+  console.log(`Hello ${answers.name}! You chose ${answers.language}.`);
+}
+
+main();
