9d959fdd1a0b41176719331571ef25f60010205b
raja <raja.rakoto7@gmail.com>
2024-05-09 17:50:02 +0300

    core: hotfix

--- src/core/images.ts
+++ src/core/images.ts
@@ -22,18 +22,16 @@ async function sharpCompress(
 	qualityValue: number,
 	extension: T_SharpExtension,
 ): Promise<void> {
-	console.log("Starting compression ...\n");
-
 	for (const file of files) {
 		await sharp(`${INPUT_IMAGES_PATH}/${file}`)
 			[extension]({ quality: qualityValue })
 			.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
 				if (error) {
-					throw new Error(`[error]: compression failed: \n${error}`);
+					throw new Error(`[error]: minification failed: \n${error}`);
 				} else {
 					console.log(
 						chalk.green(
-							`${emoji.get("framed_picture")} ${file} compressed ... [done]`,
+							`${emoji.get("framed_picture")} ${file} minified ... [done]`,
 						),
 					);
 				}
@@ -46,20 +44,14 @@ export async function minImages(): Promise<void> {
 		...(await getImageFilesByExtension("jpg")),
 		...(await getImageFilesByExtension("jpeg")),
 	];
-	// const PNG_files = await getImageFilesByExtension("png");
-	// const WEBP_files = await getImageFilesByExtension("webp");
+	const PNG_files = await getImageFilesByExtension("png");
+	const WEBP_files = await getImageFilesByExtension("webp");
 
-	if (JPEG_files.length > 0) {
-		await sharpCompress(JPEG_files, 50, "jpeg");
-	}
+	console.log("Starting minification ...");
 
-	// if (PNG_files.length > 0) {
-	// 	await sharpCompress(PNG_files, 50, "png");
-	// }
-
-	// if (WEBP_files.length > 0) {
-	// 	await sharpCompress(WEBP_files, 50, "webp");
-	// }
+	if (JPEG_files.length > 0) await sharpCompress(JPEG_files, 50, "jpeg");
+	if (PNG_files.length > 0) await sharpCompress(PNG_files, 50, "png");
+	if (WEBP_files.length > 0) await sharpCompress(WEBP_files, 50, "webp");
 
 	restart();
 }
