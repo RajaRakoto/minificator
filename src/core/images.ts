485e65bd5d075e6c277066a6bc3713b8afe4e4ba
raja <raja.rakoto7@gmail.com>
2024-05-09 21:32:28 +0300

    core: resolve async process for sharpCompress() + refact

--- src/core/images.ts
+++ src/core/images.ts
@@ -22,36 +22,61 @@ async function sharpCompress(
 	qualityValue: number,
 	extension: T_SharpExtension,
 ): Promise<void> {
-	for (const file of files) {
-		await sharp(`${INPUT_IMAGES_PATH}/${file}`)
-			[extension]({ quality: qualityValue })
-			.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
-				if (error) {
-					throw new Error(`[error]: minification failed: \n${error}`);
-				} else {
-					console.log(
-						chalk.green(
-							`${emoji.get("framed_picture")} ${file} minified ... [done]`,
-						),
-					);
-				}
-			});
-	}
+	const successMessage = (file: string) => {
+		console.log(
+			chalk.green(`${emoji.get("framed_picture")} ${file} minified ... [done]`),
+		);
+	};
+	const errorMessage = (error: Error) => {
+		throw new Error(`[error]: minification failed: \n${error}`);
+	};
+
+	return new Promise((resolve, reject) => {
+		for (const file of files) {
+			if (extension === "jpeg" || extension === "jpg") {
+				sharp(`${INPUT_IMAGES_PATH}/${file}`)
+					.jpeg({ quality: qualityValue })
+					.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
+						error ? reject(errorMessage(error)) : resolve(successMessage(file));
+					});
+			}
+			if (extension === "png") {
+				sharp(`${INPUT_IMAGES_PATH}/${file}`)
+					.png({ quality: qualityValue })
+					.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
+						error ? reject(errorMessage(error)) : resolve(successMessage(file));
+					});
+			}
+			if (extension === "webp") {
+				sharp(`${INPUT_IMAGES_PATH}/${file}`)
+					.webp({ quality: qualityValue })
+					.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
+						error ? reject(errorMessage(error)) : resolve(successMessage(file));
+					});
+			}
+		}
+	});
 }
 
-export async function minImages(): Promise<void> {
-	const JPEG_files = [
-		...(await getImageFilesByExtension("jpg")),
-		...(await getImageFilesByExtension("jpeg")),
-	];
-	const PNG_files = await getImageFilesByExtension("png");
-	const WEBP_files = await getImageFilesByExtension("webp");
+export async function minImages(level: number = 50): Promise<void> {
+	try {
+		const JPEG_files = [
+			...(await getImageFilesByExtension("jpg")),
+			...(await getImageFilesByExtension("jpeg")),
+		];
+		const PNG_files = await getImageFilesByExtension("png");
+		const WEBP_files = await getImageFilesByExtension("webp");
 
-	console.log("Starting minification ...");
+		console.log("Starting minification ...");
 
-	if (JPEG_files.length > 0) await sharpCompress(JPEG_files, 50, "jpeg");
-	if (PNG_files.length > 0) await sharpCompress(PNG_files, 50, "png");
-	if (WEBP_files.length > 0) await sharpCompress(WEBP_files, 50, "webp");
+		if (JPEG_files.length > 0) await sharpCompress(JPEG_files, level, "jpeg");
+		if (PNG_files.length > 0) await sharpCompress(PNG_files, level, "png");
+		if (WEBP_files.length > 0) await sharpCompress(WEBP_files, level, "webp");
 
-	restart();
+		restart();
+	} catch (error) {
+		throw new Error(
+			`[error]: an error occurred during minification process: \n${error}`,
+		);
+	}
 }
