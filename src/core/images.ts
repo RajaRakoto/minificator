e23d37783a180ca9decce37f66469ce3dedc9312
raja <raja.rakoto7@gmail.com>
2024-05-09 23:22:05 +0300

    core: init extension_prompt + quality_prompt | valid sharpCompress() asyn...

--- src/core/images.ts
+++ src/core/images.ts
@@ -1,4 +1,5 @@
 /* libs */
+import inquirer from "inquirer";
 import sharp from "sharp";
 import chalk from "chalk";
 import * as emoji from "node-emoji";
@@ -17,6 +18,56 @@ import { T_SharpExtension } from "@/@types";
 
 // ==============================
 
+const extension_prompt = [
+	{
+		type: "checkbox",
+		name: "extension",
+		message: "Select image extensions to minify",
+		choices: [
+			{
+				name: "JPEG",
+				value: "jpeg",
+			},
+			{
+				name: "PNG",
+				value: "png",
+			},
+			{
+				name: "WEBP",
+				value: "webp",
+			},
+		],
+		validate: (answer: string): string | boolean => {
+			if (answer.length < 1) {
+				return "You must choose at least one extension !";
+			}
+			return true;
+		},
+	},
+];
+
+const quality_prompt = [
+	{
+		type: "list",
+		name: "quality",
+		message: "Enter the quality level",
+		choices: [
+			{
+				name: "High Compression | Slight Quality Loss",
+				value: 30,
+			},
+			{
+				name: "Balanced Compression | Recommended",
+				value: 50,
+			},
+			{
+				name: "Low Compression | High Quality",
+				value: 70,
+			},
+		],
+	},
+];
+
 async function sharpCompress(
 	files: string[],
 	qualityValue: number,
@@ -31,41 +82,64 @@ async function sharpCompress(
 		throw new Error(`[error]: minification failed: \n${error}`);
 	};
 
-	return new Promise((resolve, reject) => {
-		for (const file of files) {
-			if (extension === "jpeg" || extension === "jpg") {
-				sharp(`${INPUT_IMAGES_PATH}/${file}`)
-					.jpeg({ quality: qualityValue })
-					.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
-						error ? reject(errorMessage(error)) : resolve(successMessage(file));
+	const promises = files.map((file) => {
+		return new Promise<void>((resolve, reject) => {
+			let transform: sharp.Sharp;
+			switch (extension) {
+				case "jpeg":
+					transform = sharp(`${INPUT_IMAGES_PATH}/${file}`).jpeg({
+						quality: qualityValue,
 					});
-			}
-			if (extension === "png") {
-				sharp(`${INPUT_IMAGES_PATH}/${file}`)
-					.png({ quality: qualityValue })
-					.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
-						error ? reject(errorMessage(error)) : resolve(successMessage(file));
+					break;
+				case "png":
+					transform = sharp(`${INPUT_IMAGES_PATH}/${file}`).png({
+						quality: qualityValue,
 					});
-			}
-			if (extension === "webp") {
-				sharp(`${INPUT_IMAGES_PATH}/${file}`)
-					.webp({ quality: qualityValue })
-					.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
-						error ? reject(errorMessage(error)) : resolve(successMessage(file));
+					break;
+				case "webp":
+					transform = sharp(`${INPUT_IMAGES_PATH}/${file}`).webp({
+						quality: qualityValue,
 					});
+					break;
+				default:
+					reject(new Error("[error]: unsupported extension"));
+					return;
 			}
-		}
+
+			transform.toFile(`${OUTPUT_IMAGES_PATH}/${file}`, (error) => {
+				if (error) {
+					reject(errorMessage(error));
+				} else {
+					successMessage(file);
+					resolve();
+				}
+			});
+		});
 	});
+
+	await Promise.all(promises);
 }
 
-export async function minImages(level: number = 50): Promise<void> {
+export async function minImages(): Promise<void> {
 	try {
-		const JPEG_files = [
-			...(await getImageFilesByExtension("jpg")),
-			...(await getImageFilesByExtension("jpeg")),
-		];
-		const PNG_files = await getImageFilesByExtension("png");
-		const WEBP_files = await getImageFilesByExtension("webp");
+		const extension_answers = await inquirer.prompt(extension_prompt);
+		const quality_answers = await inquirer.prompt(quality_prompt);
+		const level = quality_answers.quality;
+
+		let JPEG_files: string[] = [];
+		let PNG_files: string[] = [];
+		let WEBP_files: string[] = [];
+
+		if (extension_answers.extension.includes("jpeg")) {
+			JPEG_files = [
+				...(await getImageFilesByExtension("jpg")),
+				...(await getImageFilesByExtension("jpeg")),
+			];
+		}
+		if (extension_answers.extension.includes("png"))
+			PNG_files = await getImageFilesByExtension("png");
+		if (extension_answers.extension.includes("webp"))
+			WEBP_files = await getImageFilesByExtension("webp");
 
 		console.log("Starting minification ...");
 
