45d3d22753ca7f7365f0141e7d6718e2946f700e
raja <raja.rakoto7@gmail.com>
2024-05-09 15:00:24 +0300

    constants: set images path

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,7 @@ export const DEVMODE = true;
 
 // path
 export const FONT_PATH = "./fonts/Standard.flf";
+export const INPUT_IMAGES_PATH = DEVMODE ? "./tests" : "./";
+export const OUTPUT_IMAGES_PATH = DEVMODE
+	? "./tests/minificator/images"
+	: "./minificator/images";
