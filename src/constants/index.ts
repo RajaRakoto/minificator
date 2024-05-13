49587642ef0afcc88173771ca90720d38817139d
raja <raja.rakoto7@gmail.com>
2024-05-13 08:38:08 +0300

    constants: update output var

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,9 @@ export const DEVMODE = true;
 // path
 export const FONT_PATH = "./fonts/Standard.flf";
 export const INPUT_IMAGES_PATH = DEVMODE ? "./tests" : "./";
-export const OUTPUT_IMAGES_PATH = DEVMODE
-	? "./tests/minificator/images"
-	: "./minificator/images";
+export const OUTPUT_MIN_IMAGES_PATH = DEVMODE
+	? "./tests/minificator/minified-images"
+	: "./minificator/minified-images";
+export const OUTPUT_RESIZE_IMAGES_PATH = DEVMODE
+	? "./tests/minificator/resized-images"
+	: "./minificator/resized-images";
