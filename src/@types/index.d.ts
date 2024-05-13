ba1c6158ea886f7467366ffe61d20fdb9a787881
raja <raja.rakoto7@gmail.com>
2024-05-13 08:37:19 +0300

    types: add T_Resolution

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,2 +1,3 @@
 export type T_SharpExtension = "jpg" | "jpeg" | "png" | "webp";
 export type T_ImageExtension = "jpg" | "jpeg" | "png" | "webp" | "gif" | "svg";
+export type T_Resolution = "width" | "height";
