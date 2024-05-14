d75ea2cdefb01eaf416e0067916e03c9a2080e3c
raja <raja.rakoto7@gmail.com>
2024-05-14 11:04:28 +0300

    tests: valid test func

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,14 @@
-import { sharpTest } from "@/utils/images";
+/* utils */
+import { sharpTest, svgGoTestAsync } from "@/utils/images";
 
+// ==============================
+
+// test JPEG, PNG, WEBP sharp compression
 const qualityValue = 70;
 
 sharpTest("test.jpg", qualityValue, "jpeg");
 sharpTest("test.png", qualityValue, "png");
 sharpTest("test.webp", qualityValue, "webp");
+
+// test SVGgo compression
+svgGoTestAsync();
