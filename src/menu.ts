69026ac3faec9cbadf5c49f7cf6a8c3543824927
raja <raja.rakoto7@gmail.com>
2024-05-20 16:30:41 +0300

    src: menu prompt to func

--- src/menu.ts
+++ src/menu.ts
@@ -1,31 +1,29 @@
+/* libs */
 import inquirer from "inquirer";
-import chalk from "chalk";
 import * as emoji from "node-emoji";
+import chalk from "chalk";
 
 // ==============================
 
-export const menu_prompt = [
-	{
-		type: "list",
-		name: "menu",
-		message: chalk.green("What do you want to do ..."),
-		loop: false,
-		pageSize: 15,
-		choices: [
-			new inquirer.Separator("================= images ================="),
-			{
-				name: `${emoji.get("wrench")} Minify JPEG|PNG|WEBP`,
-				value: "min-images",
-			},
-			{
-				name: `${emoji.get("wrench")} Resize JPEG|PNG|WEBP`,
-				value: "resize-images",
-			},
-			new inquirer.Separator("=========================================="),
-			{
-				name: chalk.red(`${emoji.get("door")} exit`),
-				value: "exit",
-			},
-		],
-	},
-];
+export function menu_prompt(checkResult: object[]) {
+	checkResult.push(
+		new inquirer.Separator("=========================================="),
+		{
+			name: chalk.red(`${emoji.get("door")} exit`),
+			value: "exit",
+		},
+	);
+
+	const menu_prompt = [
+		{
+			type: "list",
+			name: "menu",
+			message: chalk.green("What do you want to do ..."),
+			loop: false,
+			pageSize: 15,
+			choices: checkResult,
+		},
+	];
+
+	return menu_prompt;
+}
