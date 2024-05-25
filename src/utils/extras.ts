58b66778b5e1ef06e1e8e70ce67cb6f89c3d32fd
raja <raja.rakoto7@gmail.com>
2024-05-25 10:30:58 +0300

    utils: add successMessage() + errorMessage()

--- src/utils/extras.ts
+++ src/utils/extras.ts
@@ -3,6 +3,7 @@ import * as emoji from "node-emoji";
 import * as path from "path";
 import open from "open";
 import util from "util";
+import chalk from "chalk";
 import fs from "fs";
 import { execa } from "execa";
 
@@ -162,4 +163,29 @@ export async function readFromFileAsync(filePath: string): Promise<string> {
 	} catch (error) {
 		throw new Error(`[error]: error during reading file: \n${error}`);
 	}
+}
+
+/**
+ * @description Display success message
+ * @param file File name to display
+ * @param emojiCode Code emoji to display (framed_picture, white_check_mark ...)
+ * @param context Context message to display (minified, resized ...)
+ */
+export function successMessage(
+	file: string,
+	emojiCode: string,
+	context: string,
+): void {
+	console.log(
+		chalk.green(`${emoji.get(emojiCode)} ${file} ${context} ... [done]`),
+	);
+}
+
+/**
+ * @description Display error message
+ * @param error Error message to display
+ * @param context Context message to display (minify, resize ...)
+ */
+export function errorMessage(error: Error, context: string): void {
+	throw new Error(`[error]: ${context} failed: \n${error}`);
 }
