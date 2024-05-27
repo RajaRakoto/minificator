d202a00a159d82572c59cee6c23708372a59750e
raja <raja.rakoto7@gmail.com>
2024-05-27 13:52:35 +0300

    core: refact + apply getAllPdfFilesAsync()

--- src/core/checker.ts
+++ src/core/checker.ts
@@ -8,6 +8,7 @@ import { SUPPORTED_ALL_IMAGES_EXTENSIONS } from "@/constants";
 
 /* utils */
 import { getAllImageFilesAsync } from "@/utils/images";
+import { getAllPdfFilesAsync } from "@/utils/pdf";
 
 // ==============================
 
@@ -26,11 +27,15 @@ export async function checkerAsync(): Promise<object[]> {
 		};
 	};
 
+	// get all supported files
 	const imagesData = await getAllImageFilesAsync(
 		SUPPORTED_ALL_IMAGES_EXTENSIONS,
 	);
+	const pdfData = await getAllPdfFilesAsync();
 
+	// check if supported files are available
 	const imagesAvailable = imagesData.length > 0;
+	const pdfAvailable = pdfData.length > 0;
 
 	menu.push(
 		new inquirer.Separator("================= images ================="),
@@ -46,8 +51,13 @@ export async function checkerAsync(): Promise<object[]> {
 			`${imagesAvailable ? "" : "no image file founded"}`,
 			imagesAvailable ? true : false,
 		),
-		new inquirer.Separator("================== test =================="),
-		getMenu("Test", "", "no test file founded", false),
+		new inquirer.Separator("================== pdf ==================="),
+		getMenu(
+			"Minify PDF",
+			`${pdfAvailable ? "min-pdf" : ""}`,
+			`${pdfAvailable ? "" : "no pdf file founded"}`,
+			pdfAvailable ? true : false,
+		),
 	);
 
 	return menu;
